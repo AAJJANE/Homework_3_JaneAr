c0930378635c9d9d027a5345097f29a229f27836
Jane <jane.a.arshinova@gmail.com>
2024-02-21 11:33:19 +0300

    Initial commit

---
+++ app/src/main/java/com/aae/homework_3_janear/MainActivity.java
@@ -0,0 +1,14 @@
+package com.aae.homework_3_janear;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
